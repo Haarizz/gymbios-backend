deb87b8ac2397260b77de18f51137cf4d0c6c524
Harikrishnan T <harikrishnantnair60@gmail.com>
2025-11-18 21:09:42 +0530

    Fix database config

--- src/main/java/com/gym/MembersApiApplication.java
+++ src/main/java/com/gym/MembersApiApplication.java
@@ -9,3 +9,18 @@ public class MembersApiApplication {
         SpringApplication.run(MembersApiApplication.class, args);
     }
 }
+package com.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.gym.repository")
+@EntityScan(basePackages = "com.gym.entity")
+public class MembersApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MembersApiApplication.class, args);
+    }
+}
