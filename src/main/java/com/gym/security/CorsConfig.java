a57319d46b09c5e086284ab849d7f2506dc4b990
Harikrishnan T <harikrishnantnair60@gmail.com>
2025-11-19 18:10:19 +0530

    Fix: CORS annotation

--- src/main/java/com/gym/security/CorsConfig.java
+++ src/main/java/com/gym/security/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://gymbios-frontend.vercel.app/")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://gymbios-frontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
